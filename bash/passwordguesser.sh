d50eebdb1f56414d5ee1224e2992c0fa7d15393e
aman-1831 <a19kaur99@gmail.com@aman-1831>
2020-02-18 16:16:55 -0800

    lab 3

--- bash/passwordguesser.sh
+++ bash/passwordguesser.sh
@@ -4,37 +4,62 @@
 # it also demonstrates using conditional execution
 
 # TASK 1: Improve it by asking the user for a password guess instead of using inline literal data (i.e. TestString)
+
+testPassword=password
+
+echo "Guess a password:"
+read myPassword
+
+
 # TASK 2: Improve it by rewriting it to use the if command
 #         The if command should test if they got it right, in which case it should tell them they got it right
 #         The if command should tell them they got it wrong
+
+if [ $myPassword == $testPassword ]
+then
+  echo "Thanks."
+else
+  echo "It is Wrong Password."
+fi
+
+
 # TASK 3: Improve it by giving them 5 tries to get it right before failing
 #           *** Do not use the exit command, and do not use a loop (e.g. while or for)
 
-
-completed="String matched successfully."
-ferror="Please try again."
-referenceString="Password"
-read -s -p "Enter the String:" myString
-echo
-if [ $myString = $referenceString ]; then
-  echo "$completed"
-  echo
+if [ $myPassword == $testPassword ]
+then
+  echo "You Password is correct."
+else
+  echo "This is your 2nd chance to enter Password:"
+  read myPassword
+  if [ $myPassword == $testPassword ]
+  then
+    echo "it is correct.... Thank You."
   else
-    echo "$ferror"
-    read -s -p "Enter the String:" myString
-    if [ $myString = $referenceString ]; then
-      echo "$completed"
-      echo
+    echo "Sorry wrong password enter it again.. Remember! 3rd attempt:"
+    read myPassword
+
+    if [ $myPassword == $testPassword ]
+    then
+      echo "You entered the correct Password!! Thank You."]
     else
-      echo
-      echo "$ferror"
-      read -s -p "Enter the String:" myString
-    if [ $myString = $referenceString ]; then
-      echo "$It is completed"
-      echo
-    else
-      echo
-      echo "$ferror"
+      echo "Sorry wrong password enter it again.. Remember! 4th attempt:"
+      read myPassword
+
+      if [ $myPassword == $testPassword ]
+      then
+        echo "You entered the correct Password!! Thank You."]
+      else
+        echo "Sorry wrong password enter it again.. Remember! 5th attempt:"
+        read myPassword
+
+        if [ $myPassword == $testPassword ]
+        then
+          echo "You entered the correct Password!! Thank You."]
+        else
+          echo "No more attempts, Sorry"
+        fi
+      fi
     fi
   fi
 fi
